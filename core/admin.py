cde01e67d2b8a23d7209ef3ec76282e7fdd61e34
ibrahimlielgiz <ibrahimli.elgiz555@gmail.com>
2019-12-22 03:27:34 +0400

    models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.register(Developer)
+admin.site.register(Projects)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-class 
+class Developers(models.Model):
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255 )
+
+class Projects(models.Model):
+    project_name = models.CharField(max_length=255)
+    developer = models.ForeignKey(Developers,on_delete=models.PROTECT)
+    start_date = models.DateField()
+    end_date = models.DateField() 
